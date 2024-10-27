85b63ca7e471bb369dfe1ca4147ec68f0fd71878
ShadowDragon5 <lauriuskas@gmail.com>
2024-10-27 10:24:51 +0100

    Add LiNGAM and FCI

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,4 +7,4 @@ GROUND_TRUTH = pd.read_csv("dataset/ground_truth.csv", header=None).values
 
 
 def SHD_with_GT(prediction) -> np.float64:
-    return SHD(GROUND_TRUTH[:15], prediction[:15])
+    return SHD(GROUND_TRUTH, prediction)
